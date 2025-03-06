99685cb02cd3a771ab3a7ba0ca64a04da1a45902
Kaichot <kaiquealvesmesquita@gmail.com>
2025-03-06 19:16:53 -0300

    feat: Update User
    update route finished
    
    Author: Kaique A Mesquita(SM)

--- backend/userApi/Application/Interfaces/IUserService.cs
+++ backend/userApi/Application/Interfaces/IUserService.cs
@@ -3,4 +3,5 @@ public interface IUserService
     UserDTO RegisterUser(UserDTO userDto);
     UserDTO? GetUserDetails(int id);
     UserDTO[] GetAllUsers();
+    UserDTO UpdateUser(int id, UserDTO userDto);
 }
