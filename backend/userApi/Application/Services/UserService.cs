6aac01b9944ce5604a29acf00221a5fb883f9558
Lucas Rocha <lucasfarocha44@gmail.com>
2025-03-13 21:31:03 -0300

    feat:Authentication system
    Implemented a way to authenticate a user when doing login
    
    Lucas F. A. Rocha

--- backend/userApi/Application/Services/UserService.cs
+++ backend/userApi/Application/Services/UserService.cs
@@ -11,15 +11,42 @@ public class UserService : IUserService
 
     public UserDTO RegisterUser(UserDTO userDto)
     {
-        var user = new User { Name = userDto.Name, Email = userDto.Email };
+        var hashedPassword = BCrypt.Net.BCrypt.HashPassword(userDto.Password);
+
+        var user = new User 
+        { 
+            Name = userDto.Name, 
+            Email = userDto.Email,
+            Phone = userDto.Phone,
+            Address = userDto.Address,
+            Password = hashedPassword,
+            Photo = userDto.Photo
+        };
         _userRepository.Add(user);
-        return userDto;
+
+        return new UserDTO
+        {
+            Name = user.Name,
+            Email = user.Email,
+            Phone = user.Phone,
+            Address = user.Address,
+            Password = user.Password,
+            Photo = user.Photo
+        };
     }
 
     public UserDTO? GetUserDetails(int id)
     {
         var user = _userRepository.GetById(id);
-        return user != null ? new UserDTO { Name = user.Name, Email = user.Email } : null;
+        return user != null ? new UserDTO 
+        { 
+            Name = user.Name, 
+            Email = user.Email,
+            Phone = user.Phone,
+            Address = user.Address,
+            Password = user.Password,
+            Photo = user.Photo
+        } : null;
     }
 
     public UserDTO[] GetAllUsers()
@@ -29,34 +56,70 @@ public class UserService : IUserService
 
         foreach(var user in users)
         {
-            userDTOs.Add(new UserDTO { Id = user.Id, Name = user.Name, Email = user.Email });
+            userDTOs.Add(new UserDTO 
+            { 
+                Id = user.Id, 
+                Name = user.Name, 
+                Email = user.Email,
+                Phone = user.Phone,
+                Address = user.Address,
+                Password = user.Password,
+                Photo = user.Photo
+            });
         }
 
         return userDTOs.ToArray();
     }
-    //update
-     public UserDTO UpdateUser(int id, UserDTO userDto)
+    
+     public UserDTO? UpdateUser(int id, UserDTO userDto)
     {
+        var hashedPassword = BCrypt.Net.BCrypt.HashPassword(userDto.Password);
 
         var user = _userRepository.GetById(id);
         if(user == null){
             return null;
         }
-       user.Name = userDto.Name;
-       user.Email = userDto.Email;
+
+        user.Name = userDto.Name;
+        user.Email = userDto.Email;
+        user.Phone = userDto.Phone;
+        user.Address = userDto.Address;
+        user.Password = hashedPassword;
+        user.Photo = userDto.Photo;
         _userRepository.Update(user);
 
         return new UserDTO
-     {
-        Id = user.Id,
-        Name = user.Name,
-        Email = user.Email
-    };
+        {
+            Name = user.Name,
+            Email = user.Email,
+            Phone = user.Phone,
+            Address = user.Address,
+            Password = user.Password,
+            Photo = user.Photo
+        };
     }
 
     public void DeleteUser(int id)
     {
         var user = _userRepository.GetById(id);
         _userRepository.Delete(user);
+    }
+
+    public UserDTO? ValidateUser(string email, string password)
+    {
+        var user = _userRepository.GetByEmail(email);
+        if(user == null || !BCrypt.Net.BCrypt.Verify(password, user.Password))
+        {
+            return null;
+        }
+
+        return new UserDTO
+        {
+            Name = user.Name,
+            Email = user.Email,
+            Phone = user.Phone,
+            Address = user.Address,
+            Photo = user.Photo
+        };
     }
 }
