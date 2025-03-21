b9b3446e2940129d8335bc79d8c4f926f7f0a9af
KaiqueAMesquita <kaiquealvesmesquita@gmail.com>
2025-03-21 15:35:27 -0300

    feat:new-migration
    new migration added to update user
    
    Kaique A Mesquita(SM)

--- backend/userApi/obj/Debug/net9.0/userApi.AssemblyInfo.cs
+++ backend/userApi/obj/Debug/net9.0/userApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("userApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c0093e29bc14c69c2527549355ffa06cd8dd9ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d4c9e04997f43d37831579f96f02ba11cf7c0ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("userApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("userApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
