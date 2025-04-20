44e55d0ac8f5b05c000c7e5d636a3ad96ef39f95
DaviRKL <davirkl07@gmail.com>
2025-04-19 22:51:32 -0300

    feat: Add User Css
    Added some styling to the add user page
    
    Davi Konuma

--- backend/userApi/obj/Debug/net9.0/userApi.AssemblyInfo.cs
+++ backend/userApi/obj/Debug/net9.0/userApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("userApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+956eeecddaa4f71e599f54f50bc333fff5a18aae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+121e2fb51f769c6ffe3f7686cb99beca0bb052f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("userApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("userApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
