ad7b021cea035023ea13c524a3dc6892477e596e
LUCAS FEITOSA ALMEIDA ROCHA <0031432412020@lti.fatecso.edu.br>
2025-04-25 16:57:01 -0300

    feat:Entities and DTOs
    Created Currency and History entities and DTOs
    
    Lucas F. A. Rocha (SM)

--- backend/currencyApi/API/DTOs/AuthResponseDTO.cs
+++
@@ -1,5 +0,0 @@
-public class AuthResponseDTO
-{
-    public string Token { get; set; }
-    public UserDTO User   { get; set; }
-}
--- backend/currencyApi/API/DTOs/CurrencyDTO.cs
+++ backend/currencyApi/API/DTOs/CurrencyDTO.cs
@@ -1,4 +1,4 @@
-public class Currency
+public class CurrencyDTO
 {
     public int Id { get; set; }
     public string Name { get; set; }
