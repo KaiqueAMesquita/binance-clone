b451ec58f60b5bf6779ae2084ea9374f4f7df7b9
Lucas Rocha <lucasfarocha44@gmail.com>
2025-05-22 22:57:48 -0300

    feat:fix_database
    Fixed data source and added a new migration to change the name of the tables
    
    Lucas F. A. Rocha (SM)

---
+++ backend/currencyApi/Migrations/20250523014146_AlteredDbContext.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace currencyApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlteredDbContext : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_History_Currency_CurrencyId",
+                table: "History");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_History",
+                table: "History");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Currency",
+                table: "Currency");
+
+            migrationBuilder.RenameTable(
+                name: "History",
+                newName: "Histories");
+
+            migrationBuilder.RenameTable(
+                name: "Currency",
+                newName: "Currencies");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_History_CurrencyId",
+                table: "Histories",
+                newName: "IX_Histories_CurrencyId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Histories",
+                table: "Histories",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Currencies",
+                table: "Currencies",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Histories_Currencies_CurrencyId",
+                table: "Histories",
+                column: "CurrencyId",
+                principalTable: "Currencies",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Histories_Currencies_CurrencyId",
+                table: "Histories");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Histories",
+                table: "Histories");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Currencies",
+                table: "Currencies");
+
+            migrationBuilder.RenameTable(
+                name: "Histories",
+                newName: "History");
+
+            migrationBuilder.RenameTable(
+                name: "Currencies",
+                newName: "Currency");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Histories_CurrencyId",
+                table: "History",
+                newName: "IX_History_CurrencyId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_History",
+                table: "History",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Currency",
+                table: "Currency",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_History_Currency_CurrencyId",
+                table: "History",
+                column: "CurrencyId",
+                principalTable: "Currency",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
