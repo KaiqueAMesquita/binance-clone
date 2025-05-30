9f8f35c9da418c1202b58eef41b7ec5013a1d441
KaiqueAMesquita <kaiquealvesmesquita@gmail.com>
2025-05-30 15:40:08 -0300

    feat:auth controller routes
    authorize added to user routes
    
    Kaique A Mesquita

--- backend/userApi/API/Controllers/UserController.cs
+++ backend/userApi/API/Controllers/UserController.cs
@@ -1,5 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
-
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
 [ApiController]
 [Route("api/[controller]")]
 public class UserController : ControllerBase
@@ -18,20 +22,21 @@ public class UserController : ControllerBase
         return Ok(result);
     }
 
+    [Authorize]
     [HttpGet("{id}")]
     public IActionResult GetUserDetails(int id)
     {
         var user = _userService.GetUserDetails(id);
         return user != null ? Ok(user) : NotFound();
     }
-
+    [Authorize]
     [HttpGet]
     public IActionResult GetAllUsers()
     {
         var users = _userService.GetAllUsers();
         return Ok(users);
     }
-
+    [Authorize]
     [HttpPut("{id}")]
     public IActionResult UpdateUser(int id, UserDTO userDTO){
         if(id != userDTO.Id ){
@@ -45,6 +50,7 @@ public class UserController : ControllerBase
         return Ok(user);
     }
 
+    [Authorize]
     [HttpDelete("{id}")]
     public IActionResult DeleteUser(int id)
     {
