c356186d50898772c83aaa8aa62ef0a676e40151
Lucas Rocha <lucasfarocha44@gmail.com>
2025-10-17 18:04:41 -0300

    feat:broker_api
    Added an API to make possible for the services (other APIs) to communicate between each other
    
    Lucas F. A. Rocha (SM)

---
+++ backend/brokerApi/Core/IQueue.cs
@@ -0,0 +1,10 @@
+namespace Core
+{
+    public interface IQueue
+    {
+        string Name { get; }
+        Task EnqueueAsync(IMessage message);
+        Task<IMessage?> DequeueAsync(CancellationToken ct);
+        int Count { get; }
+    }
+}
