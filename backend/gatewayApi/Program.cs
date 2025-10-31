9b0790b188f181d3368a8ca26571469c00c6d9a5
Lucas Rocha <lucasfarocha44@gmail.com>
2025-10-31 17:39:31 -0300

    feat:gateway_api
    Added authentication on Program.cs and User routes on ocelot.json
    
    Lucas F. A. Rocha (SM)

--- backend/gatewayApi/Program.cs
+++ backend/gatewayApi/Program.cs
@@ -1,16 +1,42 @@
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add Ocelot services to the container
+builder.WebHost.UseUrls("http://localhost:5048");
+
+// Configurações do Ocelot
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+
+// Adiciona autenticação com JWT
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(builder.Configuration["Jwt:Key"]!))
+        };
+    });
+
+// Add Ocelot services to the container
+// builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+
 builder.Services.AddOcelot();
 var app = builder.Build();
 
-app.UseOcelot().Wait();
+// Middleware de autenticação/autorização ANTES do Ocelot
+app.UseAuthentication();
+app.UseAuthentication();
 
+app.UseOcelot().Wait();
 
 app.Run();
 
